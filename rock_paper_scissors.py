ebbda0f913c039003122b08f38670b68700160bf
VencesM <71686589+VencesM@users.noreply.github.com>
2022-05-13 00:00:21 -0700

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,59 @@
+import random 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Gets the input from user
+
+player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
+
+#generates random choice from the computer
+
+computer_choice = random.randint(0, 2)
+#list stores ASCII images
+
+rps = [rock, paper, scissors]  
+#list to store names to print out
+
+choices = ["rock", "paper", "scissors"]
+
+if player_choice >= 3 or player_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+  
+else:
+  print(f"Your choice ''{choices[player_choice]}'':\n {rps[player_choice]}")
+  print(f"Computer's choice ''{choices[computer_choice]}'':\n {rps[computer_choice]}")
+
+  if player_choice == 0 and computer_choice == 2:
+    print("You win!")
+  elif computer_choice == 0 and player_choice == 2:
+    print("You lose")
+  elif computer_choice > player_choice:
+    print("You lose")
+  elif player_choice > computer_choice:
+    print("You win!")
+  elif computer_choice == player_choice:
+    print("It's a draw")
